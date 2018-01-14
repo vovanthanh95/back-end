1dca7f879d6c1e6e04788650c921acf799e54bcb
vovanthanh95 <vovanthanh180295@gmail.com>
2018-01-15 02:22:34 +0700

    create_student

---
+++ db/migrate/20180113072944_create_students.rb
@@ -0,0 +1,10 @@
+class CreateStudents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.string :s_class
+
+      t.timestamps
+    end
+  end
+end
